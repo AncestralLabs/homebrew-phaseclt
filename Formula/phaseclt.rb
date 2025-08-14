4a6bda2a8810dc972464d57337091dc76e1c7ee7
ant04x <ant04x@icloud.com>
2025-08-15 00:01:49 +0200

    Updated version 0.0.1

---
+++ Formula/phaseclt.rb
@@ -0,0 +1,23 @@
+class PhaseCLT < Formula
+  desc "A tool for creating and managing Linux, DOS or macOS bootable devices"
+  homepage "https://github.com/AncestralLabs/homebrew-phaseclt"
+  version "0.0.2"
+  license :cannot_represent
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/AncestralLabs/homebrew-phaseclt/releases/download/0.0.2/phaseclt-0.0.2-darwin-arm64.zip"
+      sha256 "sha256:90acb3ba32b739af71b5ee20723da6a855f50ae04742ad26b755ae6e7c4387c2"
+    else
+      odie "This formula only supports Apple Silicon (arm64)"
+    end
+  end
+
+  def install
+    bin.install "phaseclt"
+  end
+
+  test do
+    assert_match "phaseclt", shell_output("#{bin}/phaseclt --version")
+  end
+end
