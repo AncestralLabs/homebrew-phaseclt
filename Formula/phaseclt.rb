07691bb3049f8d67238d6264e6752f9bd239992b
ant04x <ant04x@icloud.com>
2025-08-15 00:41:25 +0200

    Updated version 0.0.1-4

--- Formula/phaseclt.rb
+++ Formula/phaseclt.rb
@@ -7,14 +7,20 @@ class Phaseclt < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/AncestralLabs/homebrew-phaseclt/releases/download/0.0.2/phaseclt-0.0.2-darwin-arm64.zip"
-      sha256 "1a351ba72ebb7414648a15e96927209c6267b0626628fa2a901d890118f8f393"
+      sha256 "90acb3ba32b739af71b5ee20723da6a855f50ae04742ad26b755ae6e7c4387c2"
     else
       odie "This formula only supports Apple Silicon (arm64)"
     end
   end
 
   def install
-    bin.install "phaseclt"
+    libexec.install Dir["*"]
+
+    (bin/"phaseclt").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/phaseclt" "$@"
+    EOS
+    chmod 0755, bin/"phaseclt"
   end
 
   test do
